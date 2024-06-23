4604619b07fbe26c6fdaa8ad372579d9a57c7b0f
GujjariSaiKumar0706 <gujjarisaikumar0706@gmail.com>
2024-06-23 08:35:34 +0530

    Completed basic model of  parent page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './components/home/Home';
 import Layout from './components/layout/Layout';
 import Login from './components/login/Login';
-import Register from './components/register/Register';
 import Register from './components/register/Register';
 import Admin from './components/admin/Admin';
 import Instructor from './components/instructor/Instructor';
@@ -55,12 +53,12 @@ function App() {
       ]
     }
   ]);
-  ]);
+
 
   return (
     <div className="App">
       <RouterProvider router={router} />
-      <RouterProvider router={router} />
+      
     </div>
   );
 }
